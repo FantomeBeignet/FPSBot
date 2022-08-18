70da34b641a5c72c7dbdfdeb8657f5b8802f037d
BENE Tom <tom.bene@telecomnancy.eu>
2022-08-18 03:19:18 +0200

    [add] added function to get twitter profile picture from account link

---
+++ utils/twitter.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	twitter "github.com/g8rswimmer/go-twitter/v2"
+	"github.com/joho/godotenv"
+)
+
+type authorize struct {
+	Token string
+}
+
+func (a authorize) Add(req *http.Request) {
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+}
+
+func GetProfilePicture(profile string) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	u, err := url.Parse(profile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	name := u.Path[1:]
+	client := &twitter.Client{
+		Authorizer: authorize{
+			Token: os.Getenv("TWITTER_TOKEN"),
+		},
+		Client: http.DefaultClient,
+		Host:   "https://api.twitter.com",
+	}
+	opts := twitter.UserLookupOpts{
+		UserFields: []twitter.UserField{
+			twitter.UserFieldProfileImageURL,
+		},
+	}
+	userResponse, err := client.UserNameLookup(context.Background(), []string{name}, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return userResponse.Raw.Users[0].ProfileImageURL
+}
